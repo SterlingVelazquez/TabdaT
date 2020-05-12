e7c07f2655e14e8f04b530a26bfc190b0c9bc674
Sterling Velazquez <SterlingVelazquez@users.noreply.github.com>
2020-05-11 21:48:22 -0400

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,100 @@
-import Head from 'next/head'
+import React from 'react';
+import Link from 'next/link';
+import Head from 'next/head';
+import {database} from "../tools/database.js";
+import "./_app.js"
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+var key=0;
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+class Home extends React.Component {
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+  constructor (props) {
+    super(props);
+    this.state = {
+        tabs : ["Financial", "Medical", "Legal", "Auto", 'Personal'],
+        links : database
+      };
+  }
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
+  activeBtn() {
+    //Get the active button using a loop
+    var btns = document.getElementsByClassName("btns");
+    for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        if (current.length > 0) {
+          console.log("bye")
+          current[0].className = current[0].className.replace(" active", "");
+        } else {
+          console.log("hello")
+          btns[0].class = "btns active";
         }
+        this.className = "btns active";
+    });
+  }}
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+  render(){
+    return (
+      <div className="container">
+        <Head>
+          <title>TabdaT</title>
+          <link rel="icon" href="/favicon.ico"/>
 
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+          <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet"></link>
+          <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400&display=swap" rel="stylesheet"></link>
+          <script src="https://kit.fontawesome.com/84e39d63f3.js" crossOrigin="anonymous"></script>
+        </Head>
 
-        footer img {
-          margin-left: 0.5rem;
-        }
+        <main>
+          <p className="title">
+            TabdaT
+          </p>
 
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+          <input placeholder="Search for your links here..."></input>
 
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
+          <div className = "buttonNav">
+              {
+                this.state.tabs.slice(0,4).map( (each) => 
+                  <button className="btns" key={key++} onClick={e => this.activeBtn()}>{each}</button>
+                )
+              }
+              <i className="fas fa-plus"></i>
+          </div>
 
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
+          <div className="grid">
 
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
+          { 
+            this.state.links.map( (each) =>
+            <a style={{textDecoration:"none"}} target="_blank" rel="noopener noreferrer" key={key++} href={each.link}>
+              <img src={each.image} key={key++}></img>
+              <p className="linkNames">{each.name}</p>
+            </a>
+            )
           }
-        }
-      `}</style>
 
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
+          <button className="addLink">Add New Link</button>
+          </div>
+        </main>
 
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+        <footer> TVTech </footer>
+
+        <style jsx global>{`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+              sans-serif;
+          }
+
+          * {
+            box-sizing: border-box;
+          }
+        `}</style>
+      </div>
+    )
+  }
 }
+
+export default Home;
---
+++ tools/database.js
@@ -0,0 +1,63 @@
+var num1 = {
+    name: "Bank Of America",
+    image: "bankofamerica-icon.jpg",
+    link: "https://www.bankofamerica.com/"
+}
+
+var num2 = {
+    name: "Binance",
+    image: "binance-icon.jpg",
+    link: "https://www.binance.com/en"
+}
+
+var num3 = {
+    name: "Charles Schwab",
+    image: "charlesschwab-icon.jpg",
+    link: "https://www.schwab.com/"
+}
+
+var num4 = {
+    name: "Chase",
+    image: "chase-icon.png",
+    link: "https://www.chase.com/"
+}
+
+var num5 = {
+    name: "Coinbase",
+    image: "coinbase-icon.png",
+    link: "https://www.coinbase.com/"
+}
+
+var num6 = {
+    name: "Discover",
+    image: "discover-icon.jpg",
+    link: "https://www.discover.com/"
+}
+
+var num7 = {
+    name: "Goldman Sachs",
+    image: "goldmansachs-icon.png",
+    link: "https://www.goldmansachs.com/"
+}
+
+var num8 = {
+    name: "Morgan Stanley",
+    image: "morganstanley-icon.png",
+    link: "https://www.morganstanley.com/"
+}
+
+var num9 = {
+    name: "Paypal",
+    image: "paypal-icon.jpeg",
+    link: "https://www.paypal.com/us/home"
+}
+
+var num10 = {
+    name: "Wells Fargo",
+    image: "wellsfargo-icon.png",
+    link: "https://www.wellsfargo.com/"
+}
+
+var database = [num1, num2, num3, num4 , num5, num6, num7, num8, num9, num10];
+
+export var database;
